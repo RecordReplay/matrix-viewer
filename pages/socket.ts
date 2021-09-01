d88776a582ae3f9ada08c5787380793e5331a217
Jason Laster <jason.laster.11@gmail.com>
2021-08-31 23:07:06 -0400

    prototype

---
+++ pages/socket.ts
@@ -0,0 +1,169 @@
+import { defer, makeInfallible } from "../utils/utils";
+import {
+  ProtocolClient,
+  EventMethods,
+  EventParams,
+  CommandMethods,
+  SessionId,
+  PauseId,
+  CommandParams,
+  CommandResult,
+} from "@recordreplay/protocol";
+
+interface Message {
+  id: number;
+  method: string;
+  params: any;
+  sessionId?: string;
+  pauseId?: string;
+}
+
+let socket: WebSocket;
+let gSocketOpen = false;
+
+let gPendingMessages: Message[] = [];
+let gNextMessageId = 1;
+
+interface MessageWaiter {
+  method: string;
+  resolve: (value: any) => void;
+  reject: (reason: any) => void;
+}
+
+const gMessageWaiters = new Map<number, MessageWaiter>();
+
+// These are helpful when investigating connection speeds.
+const gStartTime = Date.now();
+let gSentBytes = 0;
+let gReceivedBytes = 0;
+
+let willClose = false;
+window.addEventListener("beforeunload", () => {
+  willClose = true;
+});
+
+export function initSocket() {
+  socket = new WebSocket("wss://dispatch.replay.io");
+
+  socket.onopen = makeInfallible(onSocketOpen);
+  socket.onclose = makeInfallible(() => onSocketClose());
+  socket.onerror = makeInfallible((evt: Event) => onSocketError(evt));
+  socket.onmessage = makeInfallible(onSocketMessage);
+}
+
+export function sendMessage<M extends CommandMethods>(
+  method: M,
+  params: CommandParams<M>,
+  sessionId?: SessionId,
+  pauseId?: PauseId
+): Promise<CommandResult<M>> {
+  const id = gNextMessageId++;
+  const msg = { id, sessionId, pauseId, method, params };
+
+  if (gSocketOpen) {
+    doSend(msg);
+  } else {
+    gPendingMessages.push(msg);
+  }
+
+  const { promise, resolve, reject } = defer<any>();
+  gMessageWaiters.set(id, { method, resolve, reject });
+
+  return promise;
+}
+
+const doSend = makeInfallible(msg => {
+  window.performance?.mark(`${msg.method}_start`);
+  const str = JSON.stringify(msg);
+  gSentBytes += str.length;
+  socket.send(str);
+});
+
+function onSocketOpen() {
+  console.log("Socket Open");
+  gPendingMessages.forEach(msg => doSend(msg));
+  gPendingMessages.length = 0;
+  gSocketOpen = true;
+}
+
+const gEventListeners = new Map<string, (ev: any) => void>();
+
+export function addEventListener<M extends EventMethods>(
+  event: M,
+  handler: (params: EventParams<M>) => void
+) {
+  if (gEventListeners.has(event)) {
+    throw new Error("Duplicate event listener: " + event);
+  }
+  gEventListeners.set(event, handler);
+}
+
+export function removeEventListener<M extends EventMethods>(event: M) {
+  gEventListeners.delete(event);
+}
+
+export const client = new ProtocolClient({
+  sendCommand: sendMessage,
+  addEventListener,
+  removeEventListener,
+});
+
+function onSocketMessage(evt: MessageEvent<any>) {
+  gReceivedBytes += evt.data.length;
+  const msg = JSON.parse(evt.data);
+
+  if (msg.id) {
+    const { method, resolve, reject } = gMessageWaiters.get(msg.id)!;
+    window.performance?.mark(`${method}_end`);
+    window.performance?.measure(method, `${method}_start`, `${method}_end`);
+
+    gMessageWaiters.delete(msg.id);
+    if (msg.error) {
+      console.warn("Message failed", method, msg.error, msg.data);
+      reject(msg.error);
+    } else {
+      resolve(msg.result);
+    }
+  } else if (gEventListeners.has(msg.method)) {
+    const handler = gEventListeners.get(msg.method)!;
+    handler(msg.params);
+  } else {
+    console.error("Received unknown message", msg);
+  }
+}
+
+function onSocketClose() {
+    log("Socket Closed");
+    gSocketOpen = false;
+}
+
+function onSocketError(evt: Event) {
+  console.error("Socket Error", evt);
+  // If the socket has errored, the connection will close. So let's set `willClose`
+  // so that we show _this_ error message, and not the `onSocketClose` error message
+}
+
+export function log(text: string) {
+  // Don't actually log anything. This is a convenient place to add a logpoint
+  // when reviewing recordings of the viewer.
+}
+
+// // Debugging methods.
+// if (typeof window === "object") {
+//   (window as any).disconnect = () => {
+//     socket.close();
+//   };
+
+//   (window as any).outstanding = () => {
+//     const messages = [...gMessageWaiters.entries()].map(([id, { method }]) => ({
+//       id,
+//       method,
+//     }));
+//     return {
+//       messages,
+//       time: Date.now() - gStartTime,
+//       sent: gSentBytes,
+//       received: gReceivedBytes,
+//     };
+//   };
+// }
---
+++ utils/utils.ts
@@ -0,0 +1,181 @@
+export function makeInfallible(fn: (...args: any[]) => void, thisv?: any) {
+  return (...args: any[]) => {
+    try {
+      fn.apply(thisv, args);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+}
+
+export interface Deferred<T> {
+  promise: Promise<T>;
+  resolve: (value: T) => void;
+  reject: (reason: any) => void;
+}
+
+export function defer<T>() {
+  let resolve!: (value: T) => void;
+  let reject!: (reason: any) => void;
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  return { promise, resolve, reject };
+}
+
+export function waitForTime(ms: number) {
+  return new Promise<void>(resolve => setTimeout(resolve, ms));
+}
+
+export function throttle(callback: () => void, time: number) {
+  let scheduled = false;
+  return () => {
+    if (scheduled) {
+      return;
+    }
+    scheduled = true;
+    setTimeout(() => {
+      scheduled = false;
+      callback();
+    }, time);
+  };
+}
+
+export function clamp(value: number, min: number, max: number) {
+  assert(min < max);
+  return Math.max(min, Math.min(max, value));
+}
+
+export function assert(condition: any, msg = "Assertion failed!"): asserts condition {
+  if (!condition) {
+    console.error(msg);
+    throw new Error(msg);
+  }
+}
+
+export function binarySearch(start: number, end: number, callback: (mid: number) => number) {
+  while (start + 1 < end) {
+    const mid = ((start + end) / 2) | 0;
+    const rv = callback(mid);
+    if (rv < 0) {
+      end = mid;
+    } else {
+      start = mid;
+    }
+  }
+  return start;
+}
+
+function NotAllowed() {
+  console.error("Not allowed");
+}
+
+export const DisallowEverythingProxyHandler: ProxyHandler<object> = {
+  // getPrototypeOf() {
+  //   NotAllowed();
+  //   return null;
+  // },
+  //has() {
+  //  NotAllowed();
+  //  return false;
+  //},
+  get() {
+    NotAllowed();
+  },
+  //set() { NotAllowed(); },
+  apply() {
+    NotAllowed();
+  },
+  construct() {
+    NotAllowed();
+    return {};
+  },
+  getOwnPropertyDescriptor() {
+    NotAllowed();
+    return undefined;
+  },
+  ownKeys() {
+    NotAllowed();
+    return [];
+  },
+  isExtensible() {
+    NotAllowed();
+    return false;
+  },
+  setPrototypeOf() {
+    NotAllowed();
+    return false;
+  },
+  preventExtensions() {
+    NotAllowed();
+    return true;
+  },
+  defineProperty() {
+    NotAllowed();
+    return false;
+  },
+  deleteProperty() {
+    NotAllowed();
+    return false;
+  },
+};
+
+export interface EventEmitter<T> {
+  eventListeners: Map<string, ((value?: T) => void)[]>;
+  on: (name: string, handler: (value?: T) => void) => void;
+  off: (name: string, handler: (value?: T) => void) => void;
+  emit: (name: string, value?: T) => void;
+}
+
+export const EventEmitter = {
+  decorate<T>(obj: EventEmitter<T>) {
+    obj.eventListeners = new Map<string, ((value?: T) => void)[]>();
+
+    obj.on = (name, handler) => {
+      if (obj.eventListeners.has(name)) {
+        obj.eventListeners.get(name)!.push(handler);
+      } else {
+        obj.eventListeners.set(name, [handler]);
+      }
+    };
+
+    obj.off = (name, handler) => {
+      obj.eventListeners.set(
+        name,
+        (obj.eventListeners.get(name) || []).filter(h => h != handler)
+      );
+    };
+
+    obj.emit = (name, ...values) => {
+      (obj.eventListeners.get(name) || []).forEach(handler => handler(...values));
+    };
+  },
+};
+
+// Map from keys to arrays of values.
+export class ArrayMap<K, V> {
+  map: Map<K, V[]>;
+
+  constructor() {
+    this.map = new Map<K, V[]>();
+  }
+
+  add(key: K, value: V) {
+    if (this.map.has(key)) {
+      this.map.get(key)!.push(value);
+    } else {
+      this.map.set(key, [value]);
+    }
+  }
+}
+
+/**
+ * Compare 2 integers encoded as numeric strings, because we want to avoid using BigInt (for now).
+ * This will only work correctly if both strings encode positive integers (without decimal places),
+ * using the same base (usually 10) and don't use "fancy stuff" like leading "+", "0" or scientific
+ * notation.
+ */
+export function compareNumericStrings(a: string, b: string) {
+  return a.length < b.length ? -1 : a.length > b.length ? 1 : a < b ? -1 : a > b ? 1 : 0;
+}
